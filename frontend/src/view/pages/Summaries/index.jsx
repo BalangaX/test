998ef2dd26ed5f8fc4925166c03684da3cbc7c06
BalangaX <orbalanga2510@gmail.com>
2025-04-24 16:29:57 +0300

    feat: complete Assignment 4

---
+++ frontend/src/view/pages/Summaries/index.jsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+export default function Summaries() {
+  return (
+    <div className={styles.container}>
+      <h1>Summaries</h1>
+    </div>
+  );
+}
---
+++ frontend/src/view/pages/WritingAssistant/index.jsx
@@ -0,0 +1,8 @@
+import styles from "./style.module.css";
+export default function WritingAssistant() {
+  return (
+    <div className={styles.container}>
+      <h1>WritingAssistant</h1>
+    </div>
+  );
+}
